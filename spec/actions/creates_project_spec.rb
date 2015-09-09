4dc7580abf41c00694cfa90e4d3c8847ee75d4ba
Jodie Alaine Parker <me@jodiealaine.com>
2015-09-09 16:13:08 +0100

    Refactor creates project spec

--- spec/actions/creates_project_spec.rb
+++ spec/actions/creates_project_spec.rb
@@ -7,39 +7,45 @@ describe CreatesProject do
 		expect(creator.project.name).to eq("Project Bob")
 	end
 
-	describe "task string parsing" do 
-		it "handles an empty string" do 
-			creator = CreatesProject.new(name: "Test", task_string: "")
-			tasks = creator.convert_string_to_tasks
-			expect(tasks.size).to eq(0)
+	describe "task string parsing" do
+		let(:creator) { CreatesProject.new(name: "Test", task_string: task_string) }
+		let(:tasks) { creator.convert_string_to_tasks } 
+
+		describe "with an empty string" do 
+			let(:task_string) { "" }
+			specify { expect(tasks.size).to eq(0) }
 		end
 
-		it "handles a single string" do 
-			creator = CreatesProject.new(name: "Test", task_string: "about things")
-			tasks = creator.convert_string_to_tasks
-			expect(tasks.size).to eq(1)
-			expect(tasks.map(&:title)).to eq(["about things"])
-			expect(tasks.map(&:size)).to eq([1])
+		describe "with a single string" do 
+			let(:task_string) { "about things" }
+			let(:tasks) { creator.convert_string_to_tasks }
+			specify { expect(tasks.size).to eq(1) }
+			specify { expect(tasks.map(&:title)).to eq(["about things"]) }
+			specify { expect(tasks.map(&:size)).to eq([1]) }
 		end
-		it "handles a single string with size" do 
-			creator = CreatesProject.new(name: "Test", task_string: "about things:3")
-			tasks = creator.convert_string_to_tasks
-			expect(tasks.size).to eq(1)
-			expect(tasks.map(&:title)).to eq(["about things"])
-			expect(tasks.map(&:size)).to eq([3])
+
+		describe "with a single string with size" do 
+			let(:task_string) { "about things:3" }
+			let(:tasks) { creator.convert_string_to_tasks }
+			specify { expect(tasks.size).to eq(1) }
+			specify { expect(tasks.map(&:title)).to eq(["about things"]) }
+			specify { expect(tasks.map(&:size)).to eq([3]) }
 		end
-		it "handles multiple tasks" do 
-			creator = CreatesProject.new(name: "Test", task_string: "about things:3\nanother things:2")
-			tasks = creator.convert_string_to_tasks
-			expect(tasks.size).to eq(2)	
-			expect(tasks.map(&:title)).to eq(["about things", "another things"])
-			expect(tasks.map(&:size)).to eq([3, 2])
+
+		describe "handles multiple tasks" do 
+			let(:task_string) { "about things:3\nanother things:2"}
+			let(:tasks) { creator.convert_string_to_tasks }
+			specify { expect(tasks.size).to eq(2)	}
+			specify { expect(tasks.map(&:title)).to eq(["about things", "another things"]) }
+			specify { expect(tasks.map(&:size)).to eq([3, 2]) }
 		end
-		it "attaches tasks to the project" do
-			creator = CreatesProject.new(name: "Test", task_string: "about things:3\nanother things:3")
-			creator.create
-			expect(creator.project.tasks.size).to eq(2)
-			expect(creator.project).not_to be_a_new_record
+		describe "attaches tasks to the project" do
+			let(:task_string) { "about things:3\nanother things:3" }
+			it "saves the project and tasks" do 
+				creator.create
+				expect(creator.project.tasks.size).to eq(2)
+				expect(creator.project).not_to be_a_new_record
+			end
 		end
 	end
 end
